ba94f82fd4cd6cb854f3c2cd63e051d6dfab0b36
MarcusAires <gadelhamarcus27@gmail.com>
2025-03-06 04:40:31 -0300

    subindo exercicios de java

---
+++ exercicios java/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int loopVar = 12;
+
+        do {
+            System.out.println("The number: " + loopVar + " gives the condition (loopVar <10) as: " + (loopVar < 10));
+        } while(loopVar < 10);
+
+
+    }
+}
